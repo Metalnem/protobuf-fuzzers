e81477086b88280003d58b8713d825a1e04442f5
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-05-14 08:49:11 +0200

    Add libfuzzer-proto-dotnet build scripts

---
+++ drivers/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -eux
+
+docker build -t libfuzzer-proto-dotnet .
+docker container create --name extract-libfuzzer-proto-dotnet libfuzzer-proto-dotnet
+docker container cp extract-libfuzzer-proto-dotnet:/app/libprotobuf-mutator/build/src/libfuzzer-proto-dotnet/libfuzzer-proto-dotnet .
+docker container rm -f extract-libfuzzer-proto-dotnet
+zip libfuzzer-proto-dotnet.zip libfuzzer-proto-dotnet
