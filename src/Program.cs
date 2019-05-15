9963d992a656b65f010a9e85a33f0e8d5c0b10c1
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-05-15 10:53:52 +0200

    Implement fuzzing Program.cs

---
+++ src/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Formatting;
+using Microsoft.CodeAnalysis.Emit;
+using Microsoft.CodeAnalysis.Formatting;
+using SharpFuzz;
+
+namespace Roslyn.Fuzz
+{
+	public class Program
+	{
+		private static readonly HashSet<string> ignore = new HashSet<string>
+		{
+			"CS0020", // Division by constant zero
+			"CS0220", // The operation overflows at compile time in checked mode
+		};
+
+		public static void Main(string[] args)
+		{
+			Fuzzer.LibFuzzer.Run(span =>
+			{
+				string code = null;
+
+				try
+				{
+					code = CodeBuilder.Build(Function.Parser.ParseFrom(span.ToArray()));
+				}
+				catch
+				{
+					return;
+				}
+
+				var tree = CSharpSyntaxTree.ParseText(code);
+
+				var options = new CSharpCompilationOptions(OutputKind.ConsoleApplication)
+					.WithConcurrentBuild(false)
+					.WithDeterministic(true);
+
+				var compilation = CSharpCompilation.Create("Roslyn.Fuzz.dll")
+					.WithOptions(options)
+					.AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location))
+					.AddSyntaxTrees(tree);
+
+				using (var memory = new MemoryStream())
+				{
+					var result = compilation.Emit(memory);
+
+					if (!result.Success && !result.Diagnostics.Any(diagnostic => ignore.Contains(diagnostic.Id)))
+					{
+						throw new Exception(FormatError(tree, result));
+					}
+				}
+			});
+		}
+
+		private static string FormatError(SyntaxTree syntaxTree, EmitResult emitResult)
+		{
+			StringBuilder sb = new StringBuilder();
+
+			foreach (var diagnostic in emitResult.Diagnostics)
+			{
+				sb.AppendLine(diagnostic.ToString());
+			}
+
+			var workspace = new AdhocWorkspace();
+
+			var options = workspace.Options
+				.WithChangedOption(CSharpFormattingOptions.WrappingKeepStatementsOnSingleLine, false)
+				.WithChangedOption(CSharpFormattingOptions.WrappingPreserveSingleLine, false);
+
+			var formattedTree = Formatter.Format(syntaxTree.GetRoot(), workspace, options);
+			sb.Append(formattedTree.ToString());
+
+			return sb.ToString();
+		}
+	}
+}
