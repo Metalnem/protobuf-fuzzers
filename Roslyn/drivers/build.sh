8813414d2d637d2670a26dc57a3746f4fcd361b6
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-05-24 09:31:55 +0200

    Merged C++ and C# build staged into single Dockerfile

--- Roslyn/drivers/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eux
-
-docker build -t libfuzzer-proto-dotnet .
-docker container create --name extract-libfuzzer-proto-dotnet libfuzzer-proto-dotnet
-docker container cp extract-libfuzzer-proto-dotnet:/app/libprotobuf-mutator/build/src/libfuzzer-proto-dotnet/libfuzzer-proto-dotnet .
-docker container rm -f extract-libfuzzer-proto-dotnet
-tar -czvf libfuzzer-proto-dotnet.tar.gz libfuzzer-proto-dotnet
