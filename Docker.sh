fa6aa5a5209dd466c5138050b0903eb2da62a84a
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-06-01 16:14:09 +0200

    Add Docker install script for Bionic

---
+++ Docker.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -eux
+
+apt update
+apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt update
+apt install -y docker-ce
