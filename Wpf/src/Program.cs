babfe6b457b8b3dde00582fa7634794d853805ed
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-06-06 08:57:10 +0200

    Untested code for testing trace stability

--- Wpf/src/Program.cs
+++ Wpf/src/Program.cs
@@ -1,18 +1,46 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Threading;
 using System.Windows;
+using SharpFuzz.Common;
 
 namespace Wpf.Fuzz
 {
 	public static class Program
 	{
 		[STAThread]
-		public static void Main(string[] args)
+		public static unsafe void Main(string[] args)
 		{
-			Window win = new Window();
-			win.Title = "Fuzzing WPF";
+			var trace = new byte[65536];
+			var sha = SHA256.Create();
 
-			Application app = new Application();
-			app.Run(win);
+			fixed (byte* ptr = trace)
+			{
+				Trace.SharedMem = ptr;
+
+				for (int i = 0; i < 100; ++i)
+				{
+					Array.Clear(trace, 0, trace.Length);
+
+					var thread = new Thread(() =>
+					{
+						var window = new Window();
+						var application = new Application();
+
+						window.Loaded += (x, y) => window.Close();
+						window.Title = "Fuzzing WPF";
+
+						application.Run(window);
+					});
+
+					thread.Join();
+
+					var hash = sha.ComputeHash(trace);
+					var hex = BitConverter.ToString(hash).Replace("-", String.Empty);
+
+					Console.WriteLine(hex);
+				}
+			}
 		}
 	}
 }
