22c100e46574ce69c9458a4398226fd57fd5730f
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-06-06 09:35:27 +0200

    Fix line endings

--- Wpf/src/Fuzz.bat
+++ Wpf/src/Fuzz.bat
@@ -1,6 +1,5 @@
 if exist out rmdir /S /Q out
 dotnet publish -r win-x64 -o out
 set SHARPFUZZ_ENABLE_ON_BRANCH_CALLBACK=true
-sharpfuzz out\PresentationCore.dll
 sharpfuzz out\PresentationFramework.dll
 out\Wpf.Fuzz.exe
