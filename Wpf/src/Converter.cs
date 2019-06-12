2861c515cbca752b74588bfc8f0aacb19f7687e6
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-06-12 10:35:04 +0200

    Add Button and TextBox

--- Wpf/src/Converter.cs
+++ Wpf/src/Converter.cs
@@ -15,6 +15,8 @@ namespace Wpf.Fuzz
 			switch (element.FrameworkelementOneofCase)
 			{
 				case Layout.FrameworkElement.FrameworkelementOneofOneofCase.TextBlock: return Convert(element.TextBlock);
+				case Layout.FrameworkElement.FrameworkelementOneofOneofCase.TextBox: return Convert(element.TextBox);
+				case Layout.FrameworkElement.FrameworkelementOneofOneofCase.Button: return Convert(element.Button);
 				case Layout.FrameworkElement.FrameworkelementOneofOneofCase.StackPanel: return Convert(element.StackPanel);
 			}
 
@@ -24,6 +26,16 @@ namespace Wpf.Fuzz
 		private static TextBlock Convert(Layout.TextBlock textBlock)
 		{
 			return new TextBlock { Text = textBlock.Text };
+		}
+
+		private static TextBox Convert(Layout.TextBox textBox)
+		{
+			return new TextBox { Text = textBox.Text };
+		}
+
+		private static Button Convert(Layout.Button button)
+		{
+			return new Button { Content = Convert(button.Content) };
 		}
 
 		private static StackPanel Convert(Layout.StackPanel stackPanel)
