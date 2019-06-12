b4abe264efa747e2b0a2e39249bcb17dc09e1db7
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-06-12 09:32:36 +0200

    Update server fuzz script

--- Wpf/Fuzz.bat
+++ Wpf/Fuzz.bat
@@ -2,4 +2,4 @@ if exist out rmdir /S /Q out
 dotnet publish -r win-x64 -o out
 sharpfuzz out\PresentationCore.dll
 sharpfuzz out\PresentationFramework.dll
-out\Wpf.Fuzz.exe
+out\Wpf.Fuzz.exe --remote
