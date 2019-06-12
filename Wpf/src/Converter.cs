634496d342829eecd58a1776a93691e067aca54e
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-06-12 11:05:13 +0200

    Add ListBox and TreeView

--- Wpf/src/Converter.cs
+++ Wpf/src/Converter.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -17,6 +18,10 @@ namespace Wpf.Fuzz
 				case Layout.FrameworkElement.FrameworkelementOneofOneofCase.TextBlock: return Convert(element.TextBlock);
 				case Layout.FrameworkElement.FrameworkelementOneofOneofCase.TextBox: return Convert(element.TextBox);
 				case Layout.FrameworkElement.FrameworkelementOneofOneofCase.Button: return Convert(element.Button);
+				case Layout.FrameworkElement.FrameworkelementOneofOneofCase.ListBoxItem: return Convert(element.ListBoxItem);
+				case Layout.FrameworkElement.FrameworkelementOneofOneofCase.ListBox: return Convert(element.ListBox);
+				case Layout.FrameworkElement.FrameworkelementOneofOneofCase.TreeViewItem: return Convert(element.TreeViewItem);
+				case Layout.FrameworkElement.FrameworkelementOneofOneofCase.TreeView: return Convert(element.TreeView);
 				case Layout.FrameworkElement.FrameworkelementOneofOneofCase.StackPanel: return Convert(element.StackPanel);
 			}
 
@@ -36,6 +41,56 @@ namespace Wpf.Fuzz
 		private static Button Convert(Layout.Button button)
 		{
 			return new Button { Content = Convert(button.Content) };
+		}
+
+		private static ListBoxItem Convert(Layout.ListBoxItem listBoxItem)
+		{
+			return new ListBoxItem { Content = Convert(listBoxItem.Content) };
+		}
+
+		private static ListBox Convert(Layout.ListBox listBox)
+		{
+			var result = new ListBox();
+
+			foreach (var child in listBox.Children)
+			{
+				if (Convert(child) is FrameworkElement element)
+				{
+					result.Items.Add(element);
+				}
+			}
+
+			return result;
+		}
+
+		private static TreeViewItem Convert(Layout.TreeViewItem treeViewItem)
+		{
+			var result = new TreeViewItem();
+
+			foreach (var child in treeViewItem.Children)
+			{
+				if (Convert(child) is FrameworkElement element)
+				{
+					result.Items.Add(element);
+				}
+			}
+
+			return result;
+		}
+
+		private static TreeView Convert(Layout.TreeView treeView)
+		{
+			var result = new TreeView();
+
+			foreach (var child in treeView.Children)
+			{
+				if (Convert(child) is FrameworkElement element)
+				{
+					result.Items.Add(element);
+				}
+			}
+
+			return result;
 		}
 
 		private static StackPanel Convert(Layout.StackPanel stackPanel)
